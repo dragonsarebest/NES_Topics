4900aec5efcbec461df456fe6453ca64a059ae01
dragonsarebest <joshuabyron61@yahoo.com>
2021-03-10 16:08:33 -0500

    util changes

--- hello.c
+++ hello.c
@@ -9,6 +9,7 @@ Finally, turn on the PPU to display video.
 #include "neslib.h"
 #include "neslib.h"
 
+#include <stdio.h>
 #include <nes.h>
 #include "vrambuf.h"
 #include "bcd.h"
@@ -17,6 +18,63 @@ Finally, turn on the PPU to display video.
 //#link "chr_generic.s"
 
 //#link "vrambuf.c"
+
+// define a 2x2 metasprite
+#define DEF_METASPRITE_2x2(name, code, attribute)\
+unsigned char name[]={\
+        0,      0,      (code)+0,   attribute, \
+        0,      8,      (code)+1,   attribute, \
+        8,      0,      (code)+2,   attribute, \
+        8,      8,      (code)+3,   attribute, \
+        128};
+
+
+
+void updateMetaSprite(unsigned char attribute, unsigned char * meta)
+{
+  //palette, is behind, flip_hor, flip_vert
+  //1 | (0 << 5) | (1 << 6) | (0 << 7);
+  //0x30, 0x20, 0x40, 0x80 ^
+  
+  meta[3] = attribute;
+  meta[7] = attribute;
+  meta[11] = attribute;
+  meta[15] = attribute;
+  
+  if(((attribute&0x40) >> 6) == 0)
+  {
+    //flip horizontally
+    meta[0] = 0;
+    meta[4] = 0;
+    meta[8] = 8;
+    meta[12] = 8;
+  }
+  else
+  {
+    //flip horizontally the other way
+    meta[0] = 8;
+    meta[4] = 8;
+    meta[8] = 0;
+    meta[12] = 0;
+  }
+  
+  if(((attribute&0x80) >> 7) == 0)
+  {
+    //flip vertically
+    meta[1] = 0;
+    meta[5] = 8;
+    meta[9] = 0;
+    meta[13] = 8;
+  }
+  else
+  {
+    //flip vertically the other way
+    meta[1] = 8;
+    meta[5] = 0;
+    meta[9] = 8;
+    meta[13] = 0;
+  }
+}
 
 typedef struct Actor
 {
@@ -27,27 +85,17 @@ typedef struct Actor
   unsigned char attribute;
 } Actor;
 
-unsigned char metasprite[]={
-        0,      0,      0xD8+0,   0, 
-        0,      8,      0xD8+1,   0, 
-        8,      0,      0xD8+2,   0, 
-        8,      8,      0xD8+3,   0, 
-        128}; //128 is the num of chars * 8 = num of bits
-
-
-char PALETTE[32] = 
+typedef struct SpriteActor
 {
-  0x03, // screen color
-  0x24, 0x16, 0x20, 0x0,// background palette 0
-  0x1c, 0x20, 0x2c, 0x0,// background palette 1
-  0x00, 0x1a, 0x20, 0x0,// background palette 2
-  0x00, 0x1a, 0x20, 0x0,// background palette 3
+  Actor act;
+  unsigned char sprite;
+} SpriteActor;
 
-  0x3F, 0x3F, 0x22, 0x0,// sprite palette 0
-  0x00, 0x37, 0x25, 0x0,// sprite palette 1
-  0x36, 0x21, 0x19, 0x0,// sprite palette 2
-  0x1d, 0x37, 0x2b,// sprite palette 3
-};
+typedef struct MetaActor
+{
+  Actor act;
+  unsigned char * metasprite;
+} MetaActor;
 
 void writeText(char * data, int addressX, int addressY)
 {
@@ -117,13 +165,34 @@ void updateScreen(unsigned char column, unsigned char row, char * buffer, unsign
   vrambuf_flush();
 }
 
+//define variables here
+DEF_METASPRITE_2x2(PlayerMetaSprite, 0xD8, 0);
+MetaActor player;
+
+DEF_METASPRITE_2x2(DoorMetaSprite, 0xC4, 0);
+MetaActor Door;
+
+char PALETTE[32] = 
+{
+  0x03, // screen color
+  0x24, 0x16, 0x20, 0x0,// background palette 0
+  0x1c, 0x20, 0x2c, 0x0,// background palette 1
+  0x00, 0x1a, 0x20, 0x0,// background palette 2
+  0x00, 0x1a, 0x20, 0x0,// background palette 3
+
+  0x3F, 0x3F, 0x22, 0x0,// sprite palette 0
+  0x00, 0x37, 0x25, 0x0,// sprite palette 1
+  0x36, 0x21, 0x19, 0x0,// sprite palette 2
+  0x1d, 0x37, 0x2b,// sprite palette 3
+};
+
 // main function, run after console reset
 void main(void) {
   
   //palette, behind, vert, horz
-  unsigned int walk_timer = 1;
+  unsigned int walk_timer = 5;
   unsigned int walk_wait = 0;
-  unsigned int door_timer = 60*2;
+  unsigned int door_timer = 60*4;
   unsigned int door_wait = 0;
   
   unsigned char floorLevel = 20;
@@ -131,11 +200,6 @@ void main(void) {
   
   unsigned int i = 0, j = 0;
   bool touchedDoor = false;
-  Actor player;
-  
-  unsigned char doorX = multiply(29,8), doorY = multiply(18,8);
-  //unsigned char doorX = 0, doorY = multiply(18,8);
-  unsigned char doorW = 16, doorH = 16;
   
   // [sprite palette_0, sprite palette_1, ?, ?, ?, behind foreground, flip hor, flip vert]
   
@@ -152,33 +216,20 @@ void main(void) {
     vram_put(floorTile);
   }
   
+  Door.act.x = 29*8;
+  Door.act.y = 18*8;
+  Door.act.dx = 0;
+  Door.act.dy = 0;
+  Door.act.attribute = 1 | (1 << 5) | (0 << 6) | (0 << 7);
+  updateMetaSprite(Door.act.attribute, DoorMetaSprite);
   
-  vram_adr(NTADR_A(29, 18));
-  vram_put(0xc4);
-  vram_put(0xc6);
-  vram_adr(NTADR_A(29, 19));
-  vram_put(0xc5);
-  vram_put(0xc7);
-  
-  //vram_adr(NTADR_A(1, 18));
-  //vram_put(0xc4);
-  //vram_put(0xc6);
-  //vram_adr(NTADR_A(1, 19));
-  //vram_put(0xc5);
-  //vram_put(0xc7);
-  
-  
-  
-  player.x = 30;
-  player.y = doorY;
-  player.dx = 1;
-  player.dy = 0;
-  player.attribute = 1 | (0 << 5) | (0 << 6) | (0 << 7);
-  metasprite[3] = player.attribute;
-  metasprite[7] = player.attribute;
-  metasprite[11] = player.attribute;
-  metasprite[15] = player.attribute;
-  
+  player.act.x = 30;
+  player.act.y = Door.act.y;
+  player.act.dx = 1;
+  player.act.dy = 0;
+  player.act.attribute = 1 | (0 << 5) | (0 << 6) | (0 << 7);
+  updateMetaSprite(player.act.attribute, PlayerMetaSprite);
+
   // enable PPU rendering (turn on screen)
   ppu_on_all();
 
@@ -186,38 +237,28 @@ void main(void) {
   while (1)
   {
     //game loop
-    char cur_oam = 0;
+    char cur_oam = 0; // max of 64 sprites on screen @ once w/out flickering
     
-    
-    //x, y, sprite number, attribute, oam
+    //x, y, sprite number, attribute, oam = sprite number
     //cur_oam = oam_spr(x, y, 0x19 ,attrib, cur_oam);
     
-    cur_oam = oam_meta_spr(player.x, player.y, cur_oam, metasprite);
+    cur_oam = oam_meta_spr(player.act.x, player.act.y, cur_oam, PlayerMetaSprite);
     
+    cur_oam = oam_meta_spr(Door.act.x, Door.act.y, cur_oam, DoorMetaSprite);
     
     walk_wait = (walk_wait+1) % walk_timer;
     //only updates every walk_timer num of frames
     
     
-    if(doIHit(player.x, player.y, doorX+8, doorY))
+    if(doIHit(player.act.x, player.act.y, Door.act.x+8, Door.act.y))
     {
       //attrib = 2 | (0 << 5) | (0 << 6) | (0 << 7);
-      player.attribute = 1 | (0 << 5) | (1 << 6) | (0 << 7);
-      metasprite[3] = player.attribute;
-      metasprite[7] = player.attribute;
-      metasprite[11] = player.attribute;
-      metasprite[15] = player.attribute;
+      //player.attribute = 1 | (0 << 5) | (1 << 6) | (0 << 7);
+      player.act.attribute = (player.act.attribute & 0xFD) | (1 << 6);
+      //1111 1101 -> removes the current value there
+      updateMetaSprite(player.act.attribute, PlayerMetaSprite);
       
-      metasprite[0] = 8;
-      metasprite[1] = 0;
-      metasprite[4] = 8;
-      metasprite[5] = 8;
-      metasprite[8] = 0;
-      metasprite[9] = 0;
-      metasprite[12] = 0;
-      metasprite[13] = 8;
-      
-      player.dx = -1;
+      player.act.dx = -1;
       updateScreen(2, 5, "On the door!", 12);
       touchedDoor = true;
     }
@@ -234,34 +275,34 @@ void main(void) {
       }
     }
     
-    if(doIHit(player.x, player.y, 0, player.y))
+    if(doIHit(player.act.x, player.act.y, 0, player.act.y))
     {
+      char str[64];
       //attrib = 0 | (0 << 5) | (1 << 6) | (0 << 7);
-      player.attribute = 1 | (0 << 5) | (0 << 6) | (0 << 7);
-      metasprite[3] = player.attribute;
-      metasprite[7] = player.attribute;
-      metasprite[11] = player.attribute;
-      metasprite[15] = player.attribute;
+      //player.act.attribute = 1 | (0 << 5) | (0 << 6) | (0 << 7);
+      sprintf(str, "%d, %d, %d, %d",((player.act.attribute&0x30)), ((player.act.attribute&0x20) >> 5), ((player.act.attribute&0x40) >> 6), ((player.act.attribute&0x80) >> 7));
+      updateScreen(2, 5, str, 12);
+      ppu_wait_frame();
+      ppu_wait_frame();
+      ppu_wait_frame();
+      ppu_wait_frame();
+      ppu_wait_frame();
+      player.act.attribute = (player.act.attribute & 0xFD);
+      //1111 1101 -> removes the current value there (should turn 1 to 0)
+      updateMetaSprite(player.act.attribute, PlayerMetaSprite);
       
-      metasprite[0] = 0;
-      metasprite[1] = 0;
-      metasprite[4] = 0;
-      metasprite[5] = 8;
-      metasprite[8] = 8;
-      metasprite[9] = 0;
-      metasprite[12] = 8;
-      metasprite[13] = 8;
-      player.dx = 1;
-      //x++;
+      player.act.dx = 1;
+      sprintf(str, "%d, %d, %d, %d",((player.act.attribute&0x30)), ((player.act.attribute&0x20) >> 5), ((player.act.attribute&0x40) >> 6), ((player.act.attribute&0x80) >> 7));
+      updateScreen(2, 5, str, 12);
     }
 
     if(walk_wait == 0)
     {
-      player.x += player.dx;
-      player.y += player.dy;
+      player.act.x += player.act.dx;
+      player.act.y += player.act.dy;
     }
     
     //this makes it wait one frame in between updates
-    ppu_wait_frame();
+    //ppu_wait_frame();
   }
 }
